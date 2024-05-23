5b7033d2496c3e493eadf69a03002e8c00e0e3fc
MISSID <Pilottik@gmail.com>
2024-05-23 20:53:55 +0300

    Fixed placing slabs But have a visual bug with item amount is 0s

--- src/main/java/missmyidmods/trowelmod/Items/Trowel.java
+++ src/main/java/missmyidmods/trowelmod/Items/Trowel.java
@@ -1,11 +1,14 @@
 package missmyidmods.trowelmod.Items;
 
 import net.minecraft.core.block.Block;
+import net.minecraft.core.block.BlockLadder;
+import net.minecraft.core.block.BlockSlab;
 import net.minecraft.core.entity.player.EntityPlayer;
 import net.minecraft.core.enums.EnumBlockSoundEffectType;
 import net.minecraft.core.item.Item;
 import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.item.block.ItemBlock;
+import net.minecraft.core.util.helper.Direction;
 import net.minecraft.core.util.helper.Side;
 import net.minecraft.core.world.World;
 
@@ -19,53 +22,34 @@ public class Trowel extends Item {
 	}
 
 	Random rand = new Random();
+
 	@Override
 	public boolean onItemUse(ItemStack itemstack, EntityPlayer player, World world, int blockX, int blockY, int blockZ, Side side, double xPlaced, double yPlaced) {
-		if(!world.isClientSide){
-			ItemStack checkedItem;
-			if(player != null){
+		if (!world.isClientSide) {
+			ItemStack checkedItem = null;
+			if (player != null) {
 				LinkedList<ItemStack> pool = new LinkedList<ItemStack>();
-				for(int i = 0;i <= 8;i++){
+				for (int i = 0; i <= 8; i++) {
 					checkedItem = player.inventory.getStackInSlot(i);
-					if(checkedItem != null && checkedItem.getItem() instanceof ItemBlock ){
-						pool.add(checkedItem);}
-				}
-				if(!pool.isEmpty()){
-					int x = blockX,y = blockY,z = blockZ;
-					switch (side){
-						case EAST:
-							x++;
-							break;
-						case WEST:
-							x--;
-							break;
-						case NORTH:
-							z--;
-							break;
-						case SOUTH:
-							z++;
-							break;
-						case TOP:
-							y++;
-							break;
-						case BOTTOM:
-							y--;
-							break;
+					if (checkedItem != null && checkedItem.getItem() instanceof ItemBlock && checkedItem.itemID != Block.ladderOak.id) {
+						pool.add(checkedItem);
 					}
+				}
+				if (!pool.isEmpty()) {
 					ItemStack chosenItem = pool.get(rand.nextInt(pool.size()));
 					if (blockY >= 0 && blockY < world.getHeightBlocks()) {
-							Block block = Block.blocksList[chosenItem.itemID];
-							if(world.canBlockBePlacedAt(chosenItem.itemID, x, y, z, false, side)) {
-								if (world.setBlockAndMetadataWithNotify(x, y, z, chosenItem.itemID, this.getPlacedBlockMetadata(chosenItem.getMetadata())) && chosenItem.consumeItem(player)) {
-									block.onBlockPlaced(world, x, y, z, side, player, yPlaced);
-									world.playBlockSoundEffect(player, ((float) x + 0.5F), ((float) y + 0.5F), ((float) z + 0.5F), block, EnumBlockSoundEffectType.PLACE);
-								}
-							}
+						if (chosenItem.stackSize != 0) {
+							chosenItem.getItem().onItemUse(chosenItem, player, world, blockX, blockY, blockZ, side, xPlaced, yPlaced);
 							return true;
-					} else return false;
+						} else {
+								pool.remove(chosenItem);
+							}
+							return false;
+						}
+					}
 				}
 			}
-		}
 		return false;
 	}
 }
+
